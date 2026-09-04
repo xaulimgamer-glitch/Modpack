326e75e6a2da03edd4f087a3e96995565e4e3c66
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-04 11:40:37 -0300

    Expand cleaver item tag

--- kubejs/server_scripts/butchery_tags.js
+++ kubejs/server_scripts/butchery_tags.js
@@ -2,6 +2,9 @@ ServerEvents.tags('item', event => {
   event.add('turipis:cleavers', [
     'butchery:bone_cleaver',
     'butchery:copper_cleaver',
-    'butchery:iron_cleaver'
+    'butchery:iron_cleaver',
+    'butchery:gold_cleaver',
+    'butchery:diamond_cleaver',
+    'butchery:netherite_cleaver'
   ])
 })
