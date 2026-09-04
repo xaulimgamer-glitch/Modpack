ca6e62245449c52ff3296464d61922c15ab3e2f3
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-03 21:51:46 -0300

    Add crude melee starter weapons

---
+++ kubejs/startup_scripts/crude_melee_weapons.js
@@ -0,0 +1,37 @@
+StartupEvents.registry('item', event => {
+  event.create('crude_long_sword', 'sword')
+    .displayName('Crude Long Sword')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('minecraft:item/stone_sword')
+
+  event.create('crude_dagger', 'sword')
+    .displayName('Crude Dagger')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('minecraft:item/wooden_sword')
+
+  event.create('crude_flanged_mace', 'sword')
+    .displayName('Crude Flanged Mace')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('minecraft:item/wooden_axe')
+
+  event.create('crude_battleaxe', 'axe')
+    .displayName('Crude Battleaxe')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('minecraft:item/wooden_axe')
+
+  event.create('crude_sword', 'sword')
+    .displayName('Crude Sword')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('minecraft:item/wooden_sword')
+
+  event.create('crude_graybeard_staff', 'sword')
+    .displayName('Crude Graybeard Staff')
+    .tier('wood')
+    .maxDamage(64)
+    .texture('irons_spellbooks:item/graybeard_staff')
+})
