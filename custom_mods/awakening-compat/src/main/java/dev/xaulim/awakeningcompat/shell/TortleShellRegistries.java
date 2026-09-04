00db6f273721fdda43d883c256dff0cf827dabc3
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-04 07:48:46 -0300

    Add Tortle shell registries

---
+++ custom_mods/awakening-compat/src/main/java/dev/xaulim/awakeningcompat/shell/TortleShellRegistries.java
@@ -0,0 +1,40 @@
+package dev.xaulim.awakeningcompat.shell;
+
+import dev.xaulim.awakeningcompat.AwakeningCompat;
+import io.github.edwinmindcraft.apoli.api.power.factory.EntityAction;
+import io.github.edwinmindcraft.apoli.api.registry.ApoliRegistries;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class TortleShellRegistries {
+
+    public static final DeferredRegister<MobEffect> MOB_EFFECTS =
+            DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, AwakeningCompat.MOD_ID);
+
+    public static final DeferredRegister<EntityAction<?>> ENTITY_ACTIONS =
+            DeferredRegister.create(ApoliRegistries.ENTITY_ACTION_KEY, AwakeningCompat.MOD_ID);
+
+    public static final RegistryObject<MobEffect> TORTLE_SHELL_EFFECT =
+            MOB_EFFECTS.register("tortle_shell", TortleShellEffect::new);
+
+    public static final RegistryObject<TortleShellAction> TORTLE_SHELL_ACTION =
+            ENTITY_ACTIONS.register("tortle_shell", TortleShellAction::new);
+
+    private TortleShellRegistries() {
+    }
+
+    public static void register(IEventBus eventBus) {
+        MOB_EFFECTS.register(eventBus);
+        ENTITY_ACTIONS.register(eventBus);
+    }
+
+    private static final class TortleShellEffect extends MobEffect {
+        private TortleShellEffect() {
+            super(MobEffectCategory.NEUTRAL, 0x5F6E4A);
+        }
+    }
+}
