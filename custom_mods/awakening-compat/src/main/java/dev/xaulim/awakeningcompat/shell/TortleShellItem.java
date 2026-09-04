7d4be6393a3c45cbb9a4a3a9a48328da3614f437
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-04 17:54:44 -0300

    Prevent dropping Tortle racial shell

--- custom_mods/awakening-compat/src/main/java/dev/xaulim/awakeningcompat/shell/TortleShellItem.java
+++ custom_mods/awakening-compat/src/main/java/dev/xaulim/awakeningcompat/shell/TortleShellItem.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
@@ -33,6 +34,14 @@ public final class TortleShellItem extends ArmorItem {
 
     @Override
     public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
+        return false;
+    }
+
+    /**
+     * The racial shell is player state rather than transferable loot.
+     */
+    @Override
+    public boolean onDroppedByPlayer(ItemStack item, Player player) {
         return false;
     }
 
