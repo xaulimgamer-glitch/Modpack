a715406271d8b006ffa47b7631b602d18ecff9f3
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-11 19:56:57 -0300

    feat(kubejs): align arch bows with longbow limbs

--- kubejs/server_scripts/awakening_arch_bow_variants.js
+++ kubejs/server_scripts/awakening_arch_bow_variants.js
@@ -1,82 +1,65 @@
 (function () {
-  var AWAKENING_ARCH_BOW_MANIFEST = 'kubejs/awakening/arch_bow_variants.json'
+  var AWAKENING_ARCH_BOW_MANIFEST_V2 = 'kubejs/awakening/arch_bow_variants.json'
 
-  function awakeningArchBowLoadData() {
-    var data = JSON.parse(JsonIO.readString(AWAKENING_ARCH_BOW_MANIFEST))
-    if (!data || data.schema !== 1 || !data.bow_types || !Array.isArray(data.materials)) {
-      throw new Error('[Awakening/ArchBows] Unsupported manifest')
-    }
+  function awakeningArchBowV2LoadData() {
+    var data = JSON.parse(JsonIO.readString(AWAKENING_ARCH_BOW_MANIFEST_V2))
+    if (!data || data.schema !== 2 || !data.bow_types || !data.assembly || !Array.isArray(data.materials)) throw new Error('[Awakening/ArchBows] Unsupported manifest')
     return data
   }
-
-  function awakeningArchBowIngredientExists(value) {
-    try {
-      return Ingredient.of(value).itemIds.size() > 0
-    } catch (error) {
-      return false
-    }
+  function awakeningArchBowV2IngredientExists(value) {
+    try { return Ingredient.of(value).itemIds.size() > 0 } catch (error) { return false }
   }
-
-  function awakeningArchBowOutput(material, typeId, type) {
+  function awakeningArchBowV2Output(material, typeId, type) {
     if (material.existing_outputs && material.existing_outputs[typeId]) return material.existing_outputs[typeId]
     return 'awakening:' + material.id + '_' + type.suffix
   }
-
-  function awakeningArchBowFindLimb(material) {
+  function awakeningArchBowV2FindLimb(material) {
     var candidates = material.limb_candidates || []
-    for (var i = 0; i < candidates.length; i++) {
-      if (awakeningArchBowIngredientExists(candidates[i])) return candidates[i]
-    }
+    for (var i = 0; i < candidates.length; i++) if (awakeningArchBowV2IngredientExists(candidates[i])) return candidates[i]
     return null
+  }
+  function awakeningArchBowV2Addition(upgrade) {
+    if (upgrade.addition_item) return { item: upgrade.addition_item }
+    if (upgrade.addition_tag) return { tag: upgrade.addition_tag.charAt(0) === '#' ? upgrade.addition_tag.substring(1) : upgrade.addition_tag }
+    throw new Error('[Awakening/ArchBows] Smithing upgrade has no addition')
   }
 
   ServerEvents.tags('item', function (event) {
-    var data = awakeningArchBowLoadData()
+    var data = awakeningArchBowV2LoadData()
     data.materials.forEach(function (material) {
-      Object.keys(data.bow_types).forEach(function (typeId) {
-        event.add('forge:tools/bows', awakeningArchBowOutput(material, typeId, data.bow_types[typeId]))
-      })
+      Object.keys(data.bow_types).forEach(function (typeId) { event.add('forge:tools/bows', awakeningArchBowV2Output(material, typeId, data.bow_types[typeId])) })
     })
   })
 
   ServerEvents.recipes(function (event) {
-    var data = awakeningArchBowLoadData()
-
+    var data = awakeningArchBowV2LoadData()
+    var installed = 0
+    var skipped = 0
     data.materials.forEach(function (material) {
+      if (material.id !== 'wood' && material.source_longbow && !awakeningArchBowV2IngredientExists(material.source_longbow)) {
+        console.warn('[Awakening/ArchBows] Skipping ' + material.id + ': source Longbow is not registered (' + material.source_longbow + ')')
+        skipped++
+        return
+      }
       Object.keys(data.bow_types).forEach(function (typeId) {
         var type = data.bow_types[typeId]
-        var output = awakeningArchBowOutput(material, typeId, type)
-
+        var output = awakeningArchBowV2Output(material, typeId, type)
         if (material.id === 'wood') return
-
         if (material.upgrade) {
           var baseMaterial = data.materials.find(function (entry) { return entry.id === material.upgrade.from })
-          if (!baseMaterial) {
-            console.warn('[Awakening/ArchBows] Missing smithing base material for ' + material.id)
-            return
-          }
-          event.custom({
-            type: 'minecraft:smithing_transform',
-            template: { item: material.upgrade.template },
-            base: { item: awakeningArchBowOutput(baseMaterial, typeId, type) },
-            addition: { tag: material.upgrade.addition.substring(1) },
-            result: { item: output }
-          }).id('awakening:arch_bows/' + material.id + '/' + type.suffix)
+          if (!baseMaterial) { console.warn('[Awakening/ArchBows] Missing smithing base material for ' + material.id); skipped++; return }
+          var baseOutput = awakeningArchBowV2Output(baseMaterial, typeId, type)
+          if (!awakeningArchBowV2IngredientExists(baseOutput)) { console.warn('[Awakening/ArchBows] Missing smithing base bow ' + baseOutput + ' for ' + material.id); skipped++; return }
+          event.custom({ type: 'minecraft:smithing_transform', template: { item: material.upgrade.template_item }, base: { item: baseOutput }, addition: awakeningArchBowV2Addition(material.upgrade), result: { item: output } }).id('awakening:arch_bows/' + material.id + '/' + type.suffix)
+          installed++
           return
         }
-
-        var limb = awakeningArchBowFindLimb(material)
-        var ingredients = []
-        if (limb) {
-          ingredients.push(limb)
-        } else {
-          for (var i = 0; i < type.material_count; i++) ingredients.push(material.ingredient)
-        }
-        ingredients.push(type.handle)
-        ingredients.push(type.string)
-
-        event.shapeless(output, ingredients).id('awakening:arch_bows/' + material.id + '/' + type.suffix)
+        var limb = awakeningArchBowV2FindLimb(material)
+        if (!limb) { console.warn('[Awakening/ArchBows] Skipping ' + material.id + ' ' + typeId + ': no registered Longbow limb from ' + JSON.stringify(material.limb_candidates || [])); skipped++; return }
+        event.shaped(output, data.assembly.pattern, { x: limb, h: type.handle, l: data.assembly.rod, s: type.string }).id('awakening:arch_bows/' + material.id + '/' + type.suffix)
+        installed++
       })
     })
+    console.info('[Awakening/ArchBows] Installed ' + installed + ' material bow recipes; skipped ' + skipped + '. Full client restart required for registry changes.')
   })
 })()
--- kubejs/startup_scripts/awakening_arch_bow_variants.js
+++ kubejs/startup_scripts/awakening_arch_bow_variants.js
@@ -1,18 +1,15 @@
 (function () {
-  var AWAKENING_ARCH_BOW_MANIFEST = 'kubejs/awakening/arch_bow_variants.json'
+  var AWAKENING_ARCH_BOW_MANIFEST_V2 = 'kubejs/awakening/arch_bow_variants.json'
 
-  function awakeningArchBowLoadData() {
-    var data = JSON.parse(JsonIO.readString(AWAKENING_ARCH_BOW_MANIFEST))
-    if (!data || data.schema !== 1 || !data.bow_types || !Array.isArray(data.materials)) {
-      throw new Error('[Awakening/ArchBows] Unsupported manifest')
-    }
+  function awakeningArchBowV2LoadData() {
+    var data = JSON.parse(JsonIO.readString(AWAKENING_ARCH_BOW_MANIFEST_V2))
+    if (!data || data.schema !== 2 || !data.bow_types || !data.assembly || !Array.isArray(data.materials)) throw new Error('[Awakening/ArchBows] Unsupported manifest')
     return data
   }
 
-  function awakeningArchBowModel(type) {
+  function awakeningArchBowV2Model(type) {
     return {
-      parent: 'item/generated',
-      textures: { layer0: type.model.texture },
+      parent: 'item/generated', textures: { layer0: type.model.texture },
       display: {
         thirdperson_righthand: { rotation: [-80, 260, -40], translation: [-1, -2, 2.5], scale: [0.9, 0.9, 0.9] },
         thirdperson_lefthand: { rotation: [-80, -280, 40], translation: [-1, -2, 2.5], scale: [0.9, 0.9, 0.9] },
@@ -28,27 +25,17 @@
   }
 
   StartupEvents.registry('item', function (event) {
-    var data = awakeningArchBowLoadData()
-
+    var data = awakeningArchBowV2LoadData()
     data.materials.forEach(function (material) {
       if (material.existing_outputs) return
-
       Object.keys(data.bow_types).forEach(function (typeId) {
         var type = data.bow_types[typeId]
         var itemId = 'awakening:' + material.id + '_' + type.suffix
-
-        event.create(itemId, 'bow')
-          .displayName(material.name + ' ' + type.name)
-          .maxDamage(type.max_damage)
-          .modelJson(awakeningArchBowModel(type))
-          .bow(function (bow) {
-            bow.modifyBow(function (attributes) {
-              attributes
-                .fullChargeTick(type.draw_time)
-                .arrowSpeed(type.velocity)
-                .baseDamage(type.base_damage)
-            })
+        event.create(itemId, 'bow').displayName(material.name + ' ' + type.name).maxDamage(type.max_damage).modelJson(awakeningArchBowV2Model(type)).bow(function (bow) {
+          bow.modifyBow(function (attributes) {
+            attributes.fullChargeTick(type.draw_time).arrowSpeed(type.velocity).baseDamage(type.base_damage)
           })
+        })
       })
     })
   })
