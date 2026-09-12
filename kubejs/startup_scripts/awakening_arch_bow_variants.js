69626603f2d499e808915947b6bbbc0a594446d7
xaulimgamer-glitch <xaulimgamer@gmail.com>
2026-09-11 21:54:27 -0300

    feat(kubejs): add awakening bow visual models

--- kubejs/startup_scripts/awakening_arch_bow_variants.js
+++ kubejs/startup_scripts/awakening_arch_bow_variants.js
@@ -1,37 +1,33 @@
 (function () {
-  var AWAKENING_ARCH_BOW_MANIFEST_V2 = 'kubejs/awakening/arch_bow_variants.json'
-
-  function awakeningArchBowV2LoadData() {
-    var data = JSON.parse(JsonIO.readString(AWAKENING_ARCH_BOW_MANIFEST_V2))
+  var MANIFEST = 'kubejs/awakening/arch_bow_variants.json'
+  function loadData() {
+    var data = JSON.parse(JsonIO.readString(MANIFEST))
     if (!data || data.schema !== 2 || !data.bow_types || !data.assembly || !Array.isArray(data.materials)) throw new Error('[Awakening/ArchBows] Unsupported manifest')
     return data
   }
-
-  function awakeningArchBowV2Model(type) {
+  function model(typeId) {
+    var child = 'awakening:item/bows/' + typeId + '_pulling_'
     return {
-      parent: 'item/generated', textures: { layer0: type.model.texture },
-      display: {
-        thirdperson_righthand: { rotation: [-80, 260, -40], translation: [-1, -2, 2.5], scale: [0.9, 0.9, 0.9] },
-        thirdperson_lefthand: { rotation: [-80, -280, 40], translation: [-1, -2, 2.5], scale: [0.9, 0.9, 0.9] },
-        firstperson_righthand: { rotation: [0, -90, 25], translation: [1.13, 3.2, 1.13], scale: [0.68, 0.68, 0.68] },
-        firstperson_lefthand: { rotation: [0, 90, -25], translation: [1.13, 3.2, 1.13], scale: [0.68, 0.68, 0.68] }
-      },
+      parent: child + '0',
       overrides: [
-        { predicate: { pulling: 1 }, model: type.model.pulling[0] },
-        { predicate: { pulling: 1, pull: 0.65 }, model: type.model.pulling[1] },
-        { predicate: { pulling: 1, pull: 0.9 }, model: type.model.pulling[2] }
+        { predicate: { pulling: 1 }, model: child + '0' },
+        { predicate: { pulling: 1, pull: 0.65 }, model: child + '1' },
+        { predicate: { pulling: 1, pull: 0.9 }, model: child + '2' }
       ]
     }
   }
-
   StartupEvents.registry('item', function (event) {
-    var data = awakeningArchBowV2LoadData()
+    var data = loadData()
     data.materials.forEach(function (material) {
       if (material.existing_outputs) return
       Object.keys(data.bow_types).forEach(function (typeId) {
         var type = data.bow_types[typeId]
-        var itemId = 'awakening:' + material.id + '_' + type.suffix
-        event.create(itemId, 'bow').displayName(material.name + ' ' + type.name).maxDamage(type.max_damage).modelJson(awakeningArchBowV2Model(type)).bow(function (bow) {
+        var item = event.create('awakening:' + material.id + '_' + type.suffix, 'bow')
+          .displayName(material.name + ' ' + type.name)
+          .maxDamage(type.max_damage)
+          .modelJson(model(typeId))
+        if (material.color) item.color(0, parseInt(material.color, 16))
+        item.bow(function (bow) {
           bow.modifyBow(function (attributes) {
             attributes.fullChargeTick(type.draw_time).arrowSpeed(type.velocity).baseDamage(type.base_damage)
           })
